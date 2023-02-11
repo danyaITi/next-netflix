1108dd0805341c69ba7b4b226652962d80f6103e
danyaITi <danil.lenskiy.1997@mail.ru>
2023-02-11 18:44:39 +0300

    Проект добавлен в репозиторий

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const withTM = require('next-transpile-modules')([
+    '@stripe/firestore-stripe-payments',
+  ])
+  
+  module.exports = withTM({
+    reactStrictMode: true,
+    images: {
+      domains: ['rb.gy', 'image.tmdb.org'],
+    },
+})
