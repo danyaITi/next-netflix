3cb0b748e8554b486f0d4c5e7db8c2c67099c0cd
danyaITi <danil.lenskiy.1997@mail.ru>
2023-02-22 16:09:41 +0300

    clean

--- pages/account.tsx
+++ pages/account.tsx
@@ -57,7 +57,6 @@ function Account({ products }: Props) {
 
         <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0 md:pb-0">
           <h4 className="text-lg text-[gray]">Plan Details</h4>
-          {/* Find the current plan */}
           <div className="col-span-2 font-medium">
             {
               products.filter(
